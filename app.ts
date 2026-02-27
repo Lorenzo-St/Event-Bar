c255e711af5384166803330a084c781d8398eaa0
Lorenzo-St <Strea.lorenzo@gmail.com>
2026-02-27 10:04:59 -0800

    trying to fix widgets

--- app.ts
+++ app.ts
@@ -1,12 +1,15 @@
 import app from "ags/gtk4/app"
+import Gdk from "gi://Gdk"
 import style from "./style.scss"
 import Bar from "./widget/Bar"
+
+const display = Gdk.Display.get_default()!
+const monitorManager = display.get_monitors()
 
 let bars = new Map<Gdk.Monitor, any>()
 
 function addBar(monitor: Gdk.Monitor) {
   if (bars.has(monitor)) return
-
   const bar = Bar(monitor)
   bars.set(monitor, bar)
 }
@@ -14,7 +17,6 @@ function addBar(monitor: Gdk.Monitor) {
 function removeBar(monitor: Gdk.Monitor) {
   const bar = bars.get(monitor)
   if (!bar) return
-
   bar.destroy()
   bars.delete(monitor)
 }
@@ -23,19 +25,34 @@ app.start({
   css: style,
 
   main() {
-    // Create bars for existing monitors
-    for (const monitor of app.get_monitors()) {
+    // Add existing monitors
+    for (let i = 0; i < monitorManager.get_n_items(); i++) {
+      const monitor = monitorManager.get_item(i) as Gdk.Monitor
       addBar(monitor)
     }
 
-    // Add new monitor
-    app.connect("monitor-added", (_, monitor) => {
-      addBar(monitor)
-    })
-
-    // Remove disconnected monitor
-    app.connect("monitor-removed", (_, monitor) => {
-      removeBar(monitor)
+    // Monitor added
+    monitorManager.connect("items-changed", () => {
+      syncMonitors()
     })
   },
 })
+
+function syncMonitors() {
+  const current = new Set<Gdk.Monitor>()
+
+  for (let i = 0; i < monitorManager.get_n_items(); i++) {
+    const monitor = monitorManager.get_item(i) as Gdk.Monitor
+    current.add(monitor)
+
+    if (!bars.has(monitor)) {
+      addBar(monitor)
+    }
+  }
+
+  for (const monitor of bars.keys()) {
+    if (!current.has(monitor)) {
+      removeBar(monitor)
+    }
+  }
+}
